a1982f06c2b58f20b2cd2750815be12009ca682b
Kambiz Frounchi <kambiz.frounchi@gmail.com>
2020-08-04 22:31:48 -0400

    Add html routes.

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,5 +1,13 @@
-handleHtmlRoutes = (app) => {
+const path = require(`path`);
 
+handleHtmlRoutes = (app) => {
+    app.get(`/notes`, (req, res) => {
+        res.sendFile(path.join(__dirname, `../public/notes.html`));
+    });
+
+    app.get(`*`, (req, res) => {
+        res.sendFile(path.join(__dirname, `../public/index.html`));
+    });
 };
 
 module.exports = handleHtmlRoutes;
