b38e4da17aead2429a5a467bbd3a586d83aba419
Kambiz Frounchi <kambiz.frounchi@gmail.com>
2020-08-05 23:42:51 -0400

    accidentally erased fix for index.js not found issue

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require(`express`);
+const path = require(`path`);
 
 const PORT = process.env.PORT || 8080;
 
@@ -7,6 +8,9 @@ const app = express();
 //set up the express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//set up the express app to serve static files (without this index.js and styles.css won't be served)
+app.use(express.static(path.join(__dirname, `public`)));
 
 //handle API and HTML routes
 require(`./routes/apiRoutes`) (app);
@@ -17,4 +21,3 @@ require(`./routes/htmlRoutes`) (app);
 app.listen(PORT, () => {
     console.log("App listening on PORT: " + PORT);
 });
-
